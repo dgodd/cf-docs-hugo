972038816d62f026ddc03fd64b0a924b4007b51d
Dave Goddard <dave@goddard.id.au>
2016-10-03 16:33:52 -0400

    output menu structure

--- scripts/convert.rb
+++ scripts/convert.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
+require 'yaml'
 
-## Rename
+# Rename
 Dir["content/**/*.md.erb"].each do |fname|
   newname = fname.gsub(/\.erb$/,'')
   File.rename(fname, newname)
@@ -11,7 +12,20 @@ Dir["content/**/*.html.md"].each do |fname|
 end
 
 Dir["content/**/*.md"].each do |fname|
-  data = File.read(fname)
-  data.gsub!(/<%= modified_date %>/, '')
-  File.write(fname, data)
+  begin
+    data = File.read(fname).split(/^---$/, 3)
+    data.shift
+    hash = YAML.load(data[0])
+    identifier = fname.gsub(/^content\//, '').gsub(/\.md/, '').gsub(/\/index$/, '')
+    hash["menu"] = { "main" => { "Name" => hash["title"], "identifier" => identifier, "parent" => identifier.gsub(/[^\/]+$/,'').gsub(/\/$/,'') } }
+    data[0] = YAML.dump(hash)
+    data[1].gsub!(/<%= modified_date %>/, '')
+    data[1].gsub!(/<strong><\/strong>/, '')
+    puts fname
+    # puts "\n\n\n----------- #{fname} ---------\n"
+    # puts data.join('---')
+    File.write(fname, data.join('---'))
+  rescue => e
+    puts "----------- WOOPS ----------- #{fname} --------"
+  end
 end
