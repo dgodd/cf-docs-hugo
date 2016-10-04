3b2276e25fd989018c949a18b1a376901e882e3a
Dave Goddard <dave@goddard.id.au>
2016-10-03 22:38:44 -0400

    Convert content

--- scripts/convert.rb
+++ scripts/convert.rb
@@ -16,11 +16,15 @@ Dir["content/**/*.md"].each do |fname|
     data = File.read(fname).split(/^---$/, 3)
     data.shift
     hash = YAML.load(data[0])
+    name = hash["title"]
+    name = identifier.gsub(/.*\//, '').capitalize if name.downcase == 'index'
     identifier = fname.gsub(/^content\//, '').gsub(/\.md/, '').gsub(/\/index$/, '')
     hash["menu"] = { "main" => { "Name" => hash["title"], "identifier" => identifier, "parent" => identifier.gsub(/[^\/]+$/,'').gsub(/\/$/,'') } }
     data[0] = YAML.dump(hash)
     data[1].gsub!(/<%= modified_date %>/, '')
     data[1].gsub!(/<strong><\/strong>/, '')
+    data[1].gsub!(/\/index\.html/, '')
+    data[1].gsub!(/\.html/, '')
     puts fname
     # puts "\n\n\n----------- #{fname} ---------\n"
     # puts data.join('---')
