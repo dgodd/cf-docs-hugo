56d3cf34929fc4358f529bb75046a6875eaefba5
Dave Goddard <dave@goddard.id.au>
2016-10-03 23:12:27 -0400

    Modify

--- scripts/convert.rb
+++ scripts/convert.rb
@@ -12,24 +12,29 @@ Dir["content/**/*.html.md"].each do |fname|
 end
 
 Dir["content/**/*.md"].each do |fname|
+  # next unless fname == 'content/bosh/index.md'
   begin
     data = File.read(fname).split(/^---$/, 3)
     data.shift
     hash = YAML.load(data[0])
+    identifier = fname.gsub(/^content\//, '').gsub(/\.md/, '').gsub(/\/index$/, '')
     name = hash["title"]
     name = identifier.gsub(/.*\//, '').capitalize if name.downcase == 'index'
-    identifier = fname.gsub(/^content\//, '').gsub(/\.md/, '').gsub(/\/index$/, '')
-    hash["menu"] = { "main" => { "Name" => hash["title"], "identifier" => identifier, "parent" => identifier.gsub(/[^\/]+$/,'').gsub(/\/$/,'') } }
+    hash["menu"] = { "main" => { "Name" => name, "identifier" => identifier, "parent" => identifier.gsub(/[^\/]+$/,'').gsub(/\/$/,'') } }
     data[0] = YAML.dump(hash)
     data[1].gsub!(/<%= modified_date %>/, '')
     data[1].gsub!(/<strong><\/strong>/, '')
     data[1].gsub!(/\/index\.html/, '')
     data[1].gsub!(/\.html/, '')
+    data[1].gsub!(/^(#+)\s*<a.*><\/a>\s*/, '\1 ')
+    data[1].gsub!(/^(#+)\s*(.*?)\s*\1$/, '\1 \2')
+
     puts fname
     # puts "\n\n\n----------- #{fname} ---------\n"
     # puts data.join('---')
     File.write(fname, data.join('---'))
   rescue => e
     puts "----------- WOOPS ----------- #{fname} --------"
+    puts e
   end
 end
